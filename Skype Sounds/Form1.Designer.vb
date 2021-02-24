5a32763322dc5c91d13478b24b9139090d9693e7
Konstantinos Drakontidis <gedra100sh@gmail.com>
2021-02-24 15:31:30 +0200

    Commit #1

---
+++ Skype Sounds/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.duplicate = New System.Windows.Forms.Button()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'BackgroundWorker1
+        '
+        '
+        'duplicate
+        '
+        Me.duplicate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.duplicate.Location = New System.Drawing.Point(12, 12)
+        Me.duplicate.Name = "duplicate"
+        Me.duplicate.Size = New System.Drawing.Size(95, 43)
+        Me.duplicate.TabIndex = 0
+        Me.duplicate.Text = "Duplicate local resource file"
+        Me.duplicate.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1077, 680)
+        Me.Controls.Add(Me.duplicate)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Skype Sound Changer"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents duplicate As Button
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Skype Sounds/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.ComponentModel
+Imports System.Management.Automation
+Imports System.Management.Automation.Runspaces
+Imports System.Text
+Public Class Form1
+
+    Public prefix As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Environment.Is64BitOperatingSystem Then
+            prefix = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+        Else
+            prefix = Environment.SpecialFolder.ProgramFiles
+        End If
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    'Pipeline - PowerShell
+    Private Function pipe(ByVal script As String) As String
+        Dim runspace As Runspace = RunspaceFactory.CreateRunspace()
+        runspace.Open()
+        Dim pipeline As Pipeline = runspace.CreatePipeline()
+        pipeline.Commands.AddScript(script)
+        pipeline.Commands.Add("Out-String")
+        Dim results As Collection(Of PSObject) = pipeline.Invoke()
+        runspace.Close()
+        Dim stringBuilder As StringBuilder = New StringBuilder()
+        For Each ps As PSObject In results
+            stringBuilder.AppendLine(ps.ToString())
+        Next
+        Return stringBuilder.ToString
+    End Function
+
+    'Checks if npm is installed and works on PowerShell
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Try
+            pipe("npx")
+            duplicate.BackColor = Color.Green
+            BackgroundWorker2.RunWorkerAsync()
+        Catch ex As Exception
+            Enabled = False
+            MsgBox("You do not have node.js installed. Please install it!" & vbNewLine & "Exiting...")
+            Close()
+        End Try
+    End Sub
+
+    Private Sub BackgroundWorker2_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker2.DoWork
+        If System.IO.File.Exists(prefix & "\Microsoft\Skype for Desktop\resources\app.asar") Then
+            MsgBox("asar located!")
+        Else
+            MsgBox("No Skype for Desktop installation could be found!")
+            MsgBox(prefix & "Microsoft\Skype for Desktop\resources\app.asar")
+        End If
+    End Sub
+End Class
